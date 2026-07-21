a933c8f0d906a4eee578eed86d9692c1f1fcf067
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-21 05:37:19 +0530

    Add files via upload

---
+++ powerOfNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter base :";
+    cin>>a;
+    cout<<"Enter exponent :";
+    cin>>b;
+    int power=1;
+    for(int i=1; i<=b; i++){
+    power= power*a;
+    }
+    cout<<a<<" raised to the power "<<b<<" is = "<<power;
+}
+// take two numbers and wap to find the value of one number raised to the power of another 
