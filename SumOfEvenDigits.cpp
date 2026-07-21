ffee124597e1e7b617b9fd326d14ae98fb79ce82
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-21 05:47:48 +0530

    Add files via upload

---
+++ SumOfEvenDigits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main(){ 
+    int n; 
+    cout<<"Enter a number :";
+    cin >> n; 
+    int sum = 0; 
+    while (n > 0){
+        int x = n % 10;
+        sum += (x % 2 == 0 ? x : 0);
+        n /= 10;
+    } 
+         cout << sum;
+    }
+    // WAP to add all the even digits of a number
