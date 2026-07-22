da713ffaac93876d93233bf68b94f0e726ffd2b0
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-23 01:32:54 +0530

    Update StarTriangleReverse.cpp

--- StarTriangleReverse.cpp
+++ StarTriangleReverse.cpp
@@ -5,7 +5,7 @@ int main(){
     cout<<"Enter number of rows:";
     cin>>n;
      for(int i=1; i<=n; i++){
-        for(int j=1; j<=n+1-i; j++){ // no of starts = n+1-i (i= row number)
+        for(int j=1; j<=n+1-i; j++){ // no of stars = n+1-i (i= row number)
             cout<<"*";
         }
         cout<<endl;
