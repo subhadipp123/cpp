c432bb5b6889edc4e3ad4514103b06ccf81c8d1d
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-23 06:52:06 +0530

    Add files via upload

---
+++ numberBridge1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of lines :";
+    cin>>n;
+    for(int i=1; i<=2*n-1; i++){
+        cout<<i; 
+    }
+    cout<<endl;
+    int m= n-1; // new lines
+    int nsp=1;
+    for(int i=1; i<=m; i++){
+        int a=1;
+        for(int j=1; j<=m+1-i; j++){
+            cout<<a;
+            a++; 
+        }
+        for(int k=1; k<=nsp; k++){
+            cout<<" "; // spaces
+        }
+        nsp+=2;
+        for(int j=1; j<=m+1-i; j++){
+            cout<<a; 
+            a++;
+        }
+        cout<<endl;
+    }
+}
