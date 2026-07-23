262ca4712f6bfb1b1c1d45f2de24e3aac4b5da1b
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-24 01:25:27 +0530

    Add files via upload

---
+++ swapWithoutVariables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first number :";
+    cin>>a;
+    cout<<"Enter second number :";
+    cin>>b;
+    a= a+b; 
+    b= a-b;
+    a= a-b;
+    cout<<" now a is "<<a<<" and b is "<<b;
+}
