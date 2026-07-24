7ce0b8564d9d0b30691a80961da2d547dc388359
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-25 02:58:04 +0530

    Add files via upload

---
+++ ClassBasics.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class player{
+    public:
+    int score;
+    int age;
+};
+int main(){
+    player Rohit;
+    Rohit.score=138;
+    Rohit.age=39;
+    cout<<Rohit.score<<endl;
+    cout<<Rohit.age<<endl;
+
+    player Virat;
+    Virat.score=74;
+    Virat.age=38;
+    cout<<Virat.score<<endl;
+    cout<<Virat.age<<endl;}
