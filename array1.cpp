51f423f0cec9e830357e6a5f16b2c3f7afbe9e7c
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-26 04:14:54 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of students :";
+    cin>>n;
+    int marks[n];
+    cout<<"Enter marks of students :";
+    for(int i=0; i<=n-1; i++){
+        cin>>marks[i];
+    }
+    for(int i=0; i<=n-1; i++){
+        if(marks[i]<=35) cout<<i<<" ";
+    }
+}
