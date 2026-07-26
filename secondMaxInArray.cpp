b2644d6208010318639eea83d1f6d53a4bc87da0
Subhadip Pradhan <subhadipmain1247@gmail.com>
2026-07-26 05:35:03 +0530

    Add files via upload

---
+++ secondMaxInArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of elements :";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<=n-1; i++){
+        cin>>arr[i];
+    }
+    int max= INT_MIN;
+    for(int i=0; i<=n-1; i++){
+        if(max<arr[i])
+        max= arr[i];
+    }
+    int smax= INT_MIN;
+    for(int i=0; i<=n-1; i++){
+        if(arr[i]!=max && smax<arr[i])
+        smax= arr[i];
+    }
+    cout<<"Max of all elements is: "<<max<<endl;
+    cout<<"Second Max of all elements is: "<<smax;
+}
